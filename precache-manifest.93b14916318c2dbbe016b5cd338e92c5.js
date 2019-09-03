d6582ded99d50a165a818cc39df7ced37553d568
nemo369 <nafrenkel@gmail.com>
2019-09-03 15:16:26 +0300

    Updates

---
+++ precache-manifest.93b14916318c2dbbe016b5cd338e92c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a0bf8c089018cc47b51a4fe7b5a0b0",
+    "url": "/naaman-frenkel-01-09-2019/index.html"
+  },
+  {
+    "revision": "5e532e3b9c689f6dadb0",
+    "url": "/naaman-frenkel-01-09-2019/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "207f872a238d0eed1813",
+    "url": "/naaman-frenkel-01-09-2019/static/css/main.888ac157.chunk.css"
+  },
+  {
+    "revision": "5e532e3b9c689f6dadb0",
+    "url": "/naaman-frenkel-01-09-2019/static/js/2.2082b80f.chunk.js"
+  },
+  {
+    "revision": "207f872a238d0eed1813",
+    "url": "/naaman-frenkel-01-09-2019/static/js/main.a5989d25.chunk.js"
+  },
+  {
+    "revision": "22315adae64d75b24aed",
+    "url": "/naaman-frenkel-01-09-2019/static/js/runtime~main.0d192674.js"
+  }
+]);
